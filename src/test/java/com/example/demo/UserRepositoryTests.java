4cd41459be1685a8ae017e8a4d6496e4f39b448b
dd <rboo666666@gmail.com>
2021-12-21 17:20:27 +0800

    demo

---
+++ src/test/java/com/example/demo/UserRepositoryTests.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    // test methods go below
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("sx@gmail.com");
+        user.setPassword("sx2020");
+        user.setFirstName("sxander");
+        user.setLastName("sbb");
+
+        User savedUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+
+    }
+
+    @Test
+    public void testFindUserByEmail(){
+        String email ="sx@gmail.com";
+
+        User user = repo.findByEmail(email);
+
+        assertThat(user).isNotNull();
+    }
+
+}
